fce3b41ccb874f5d51a55e524b05e72671d819b2
Handol Park <handol@ston-ai.com>
2025-07-05 00:05:50 +0000

    Update package requirements to allow for version ranges of numpy and tqdm
    - numpy version requirement was relaxed; numpy 1.23.5 requires disutils, which has been removed from the standard library since Python 3.12.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages(),
     license="LICENSE.txt",
     install_requires=[
-        "numpy==1.23.5",
-        "tqdm==4.62.3"
+        "numpy>=1.23.5,<2.0",
+        "tqdm>=4.62.3,<5.0"
     ],
 )
